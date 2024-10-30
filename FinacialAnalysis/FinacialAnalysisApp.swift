d91ec76b2df5933c56dadde71d2c406e1d87943f
VANN YA <vannakkm33@gmail.com>
2024-10-30 08:02:45 +0700

    Initial Commit

---
+++ FinacialAnalysis/FinacialAnalysisApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinacialAnalysisApp.swift
+//  FinacialAnalysis
+//
+//  Created by Soriya Van on 30/10/24.
+//
+
+import SwiftUI
+
+@main
+struct FinacialAnalysisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FinacialAnalysisTests/FinacialAnalysisTests.swift
@@ -0,0 +1,17 @@
+//
+//  FinacialAnalysisTests.swift
+//  FinacialAnalysisTests
+//
+//  Created by Soriya Van on 30/10/24.
+//
+
+import Testing
+@testable import FinacialAnalysis
+
+struct FinacialAnalysisTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
